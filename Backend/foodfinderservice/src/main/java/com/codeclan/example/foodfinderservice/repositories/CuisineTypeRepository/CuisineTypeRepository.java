4938cc9064ecbfb0f313e593a7b84270fca8458c
sarab78 <sarbat_kaur@hotmail.co.uk>
2019-08-10 09:47:23 +0100

    Repositories Created

---
+++ Backend/foodfinderservice/src/main/java/com/codeclan/example/foodfinderservice/repositories/CuisineTypeRepository/CuisineTypeRepository.java
@@ -0,0 +1,6 @@
+package com.codeclan.example.foodfinderservice.repositories.CuisineTypeRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuisineTypeRepository extends JpaRepository<CuisineTypeRepository, Long> {
+}
