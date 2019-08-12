705bc1c17cae11b5d9b3697f0f2d23b1d23b2063
psulli10 <petersullivan1985@gmail.com>
2019-08-12 15:09:59 +0100

    ingredients and cusinesTypes now fetched

--- frontend/food_finder/src/containers/SignUpContainer.js
+++ frontend/food_finder/src/containers/SignUpContainer.js
@@ -1,18 +1,54 @@
 import React, {Component} from 'react';
 import SignUpForm from '../components/SignUpForm'
+import Request from '../helpers/Request'
 
 class SignUpContainer extends Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      cuisineTypes: [],
+      ingredients: []
+    }
     this.createNewUser = this.createNewUser.bind(this);
   }
+
+  componentDidMount(){
+  const request = new Request()
+
+  const promise1 = request.get('/cuisineTypes');
+  const promise2 = request.get('/ingredients');
+  const promises = [promise1, promise2]
+
+  Promise.all(promises).then((data) => {
+    // debugger
+    // console.log(data);
+    this.setState({
+      cuisineTypes: data[0]._embedded.cuisineTypes,
+      ingredients: data[1]._embedded.ingredients,
+    });
+  })
+}
+
+// componentDidMount(){
+//  const getIngredients =  (() => {
+//          const url = "/ingredients"
+//          const request = new Request()
+//          request.get(url)
+//          .then(ingredients => {
+//            console.log(ingredients)
+//          }
+//        )
+//          // .then(user => this.setState({user: user}))
+//          .catch(err => console.log(err))})
+//  getIngredients()
+//  }
 
   createNewUser(user) {
       this.props.signUpMain(user);
   }
 
-// Pull ingredients and cuisine from db as list as pass as props to Form 
+// Pull ingredients and cuisine from db as list as pass as props to Form
 
   render(){
     return(
