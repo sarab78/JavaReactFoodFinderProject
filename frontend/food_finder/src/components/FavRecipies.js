09970dce9fbb0c7f1fd22933e7e242fe02677d6b
cowang01 <g_cowan27@hotmail.com>
2019-08-13 17:20:51 +0100

    startProfileListReciPIE

---
+++ frontend/food_finder/src/components/FavRecipies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const FavRecipies = (props) => {
+  return (
+    <div>
+      <h4>Your Saved Recipies</h4>
+
+    </div>
+  )
+};
+
+export default FavRecipies;
--- frontend/food_finder/src/containers/Profile.js
+++ frontend/food_finder/src/containers/Profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ProfileDetails from '../components/ProfileDetails'
+import FavRecipies from '../components/FavRecipies'
+// import FavRecipies from '../components/FavRecipies'
 import Request from '../helpers/Request'
+import Urls from '../helpers/keys/Urls'
 
 class Profile extends Component {
 
@@ -8,26 +11,38 @@ class Profile extends Component {
       super(props);
       this.state = {
         cuisineTypes: [],
-        ingredients: []
+        ingredients: [],
+        recipesFav: [],
+        restaurantFav: []
       }
       this.updateUserMain = this.updateUserMain.bind(this);
     }
 
     componentDidMount(){
+    const url = new Urls
     const request = new Request()
-
     const promise1 = request.get('/cuisineTypes');
     const promise2 = request.get('/ingredients');
     const promises = [promise1, promise2]
 
+    const recipeAPI = this.props.user.recipeId;
+    const restaurantAPI = this.props.user.restaurantId;
+
     Promise.all(promises).then((data) => {
-      // debugger
-      // console.log(data);
       this.setState({
         cuisineTypes: data[0]._embedded.cuisineTypes,
         ingredients: data[1]._embedded.ingredients,
       });
     })
+
+    let favouriteRecipies = []
+    console.log(recipeAPI);
+    {for (let l_recipe of recipeAPI){
+        fetch(url.urlRecipe() + l_recipe)
+        .then(res => res.json())
+        .then(recipe => this.setState({recipesFav: this.state.recipesFav.push(recipe)}))
+        .catch(err => console.log(err));
+    }}
   }
 
   updateUserMain(updatedUser) {
@@ -45,6 +60,7 @@ class Profile extends Component {
             <ProfileDetails user={this.props.user} cuisines={this.state.cuisineTypes} ingredients={this.state.ingredients} updateUserDetails={this.updateUserMain}/>
           </div>
           <div>
+            <FavRecipies user={this.props.user}/>
             {/* List of Reciepies */}
             {/* List of Restaurants */}
           </div>
